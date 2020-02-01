fe35e0b8ae19c3051b57b0050f1323406878b130
shunz <rockucn@gmail.com>
2020-02-02 01:24:37 +0800

    练习Turtle方法_海龟动作_移动和绘制

--- turtle/lib_turtle.py
+++ turtle/lib_turtle.py
@@ -1,5 +1,6 @@
 '''
 turtle ——— 海龟绘图
+https://docs.python.org/zh-cn/3/library/turtle.html
 
 源自1967年创造的Logo编程语言
 很适合引导孩子学习编程
@@ -21,7 +22,7 @@ from turtle import *
 - goto() | setpos() | setposition() 前往/定位
 - setx() 设置x坐标
 - sety() 设置y坐标
-- setheading() | seth() 设置朝向
+- setheading() | seth() 设置朝向 (0-东|90-北|180-西|270-南)
 - home() 返回原点
 - circle() 画圆
 - dot() 画点
@@ -31,6 +32,82 @@ from turtle import *
 - undo() 撤销
 - speed()
 '''
+
+speed('fast')
+
+print(position())
+fd(25)
+print(pos())
+fd(-75)
+print(pos())
+
+bk(30)
+print(pos())
+
+print(heading())
+rt(45)
+lt(90)
+print(heading())
+
+tp = pos()
+print(tp)
+goto(60,30) # 移至一个绝对坐标，画笔落下时将画线，不改变朝向
+setpos(20, 50)
+setposition(100, 200)
+
+setx(-50)
+sety(30)
+
+seth(0)
+setheading(90)
+print(heading())
+
+home() # 移至初始坐标，设置朝向为初始方向
+print(heading())
+print(position())
+
+'''
+circle(半径, [夹角], [边数])
+- 圆心在海龟左边
+- 海龟的朝向会随夹角参数改变
+- 圆实际是以其内切正多边形近似表示，边数参数可以用来绘制正多边形
+'''
+reset()
+speed(6)
+circle(50)
+circle(80, 180)
+circle(100, 360, 5)
+
+dot() # 绘制直径为size的圆点，若未指定，取pensize+4和2*pensize中的较大值
+dot(5,'blue') # 可指定颜色
+
+fd(30)
+color('red')
+stp_id = stamp() # 在当前位置印制一个海龟形状，返回stamp_id，可通过clearstamp(stamp_id)删除
+fd(30)
+clearstamp(stp_id)
+
+for _ in range(6):
+    stamp(); fd(30)
+
+clearstamps(2) # 删除前2个印章
+clearstamps(-2) # 删除后2个印章
+clearstamps() # 删除所有印章
+
+for _ in range(6):
+    undo() # 撤销最近的动作，可撤销次数由缓冲区大小决定
+
+'''
+Speed参数默认为None，可设0..10整数或速度字符串
+- 'fastest': 0 最快，没有动画效果(直接跳至或转向至目标位置或朝向)
+- 'fast': 10 快
+- 'normal': 6 正常
+- 'slow': 3 慢
+- 'slowest': 1 最慢
+'''
+speed('normal')
+speed(9)
+print(speed())
 
 
 
