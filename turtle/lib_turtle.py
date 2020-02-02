90bcc980abf9edc32b33fec136a199a815fb02d9
shunz <rockucn@gmail.com>
2020-02-03 02:04:23 +0800

    练习：海龟动作（获取海龟的状态、设置与度量单位），画笔控制（绘图状态、颜色控制、填充、其他绘图控制）

--- turtle/lib_turtle.py
+++ turtle/lib_turtle.py
@@ -32,7 +32,6 @@ from turtle import *
 - undo() 撤销
 - speed()
 '''
-
 speed('fast')
 
 print(position())
@@ -120,7 +119,6 @@ print(speed())
 - heading 朝向
 - distance() 距离
 '''
-
 print(position())
 print(pos())
 
@@ -134,11 +132,21 @@ print(heading()) # 海龟的朝向
 print(distance(0, 0)) # 从海龟位置到(x, y)之间的距离
 
 
+
 ### 设置与度量单位
 '''
 degrees() 角度
 radians() 弧度
 '''
+degrees(400.0) # 设置角度的度量单位，默认为360
+seth(100.0)
+degrees(360)
+print(heading()) # 90.0
+
+radians() # 设置角度的度量单位为弧度。其值等于 degrees(2*math.pi)
+heading() # 1.5707......
+degrees(360)
+
 
 
 ## 画笔状态
@@ -148,17 +156,67 @@ radians() 弧度
 - pendown() | pd() | down() 画笔落下
 - penup() | pu() | up() 画笔抬起
 - pensize() | width() 画笔粗细
-- pen() 画笔
+- pen() 返回或设置画笔属性
 - isdown() 画笔是否落下
 '''
+pensize() # 返回当前线条大小
+pensize(2)
+
+'''
+画笔属性，是一个包含以下键值对的「画笔字典」
+此字典可作为后续调用pen()时的参数，以恢复之前的画笔状态
+- 'shown': True/False
+- 'pendown': True/False
+- 'pencolor': 颜色字符串或颜色元组
+- 'fillcolor': 颜色字符串或颜色元组
+- 'pensize': 正数值
+- 'speed': 0..10 范围内的数值
+- 'resizemode': 'auto'、'user'、'noresize'
+- 'stretchfactor': (正数值, 正数值)
+- 'outline': 正数值
+- 'tilt': 数值
+'''
+pen() # 返回画笔属性，包含以上键值对的「画笔字典」
+pen(fillcolor='red', pencolor='green', pensize=3)
+sorted(pen().items())
+penstate = pen()
+color('purple','')
+penup()
+sorted(pen().items())[:3]
+pen(penstate, fillcolor='brown')
+sorted(pen().items())[:3]
+
+isdown() # 若画笔落下返回 True
+
 
 
 ### 颜色控制
 '''
 - color() 颜色
-- pencolor() 画笔颜色
-- fillcolor() 填充颜色
+- pencolor(*args) 画笔颜色
+- fillcolor(*args) 填充颜色
 '''
+pencolor() # 返回画笔颜色，可用作其他color调用的输入
+pencolor('#33cc99') # 设置画笔颜色，>>> (0.2, 0.8, 0.6)
+pencolor('red')
+
+colormode(255) # 设置颜色模式为255
+pencolor((255,255,255)) # 设置画笔颜色为以r,g,b元组表示的RGB颜色
+pencolor(128,128,128)
+
+colormode(1) # 设置颜色模式为1
+pencolor((1,1,1))
+pencolor(0.5,0.5,0.5)
+tup = (0.2, 0.8, 0.55)
+pencolor(tup)
+
+color() # 以元组返回一对颜色字符串或颜色元组表示当前画笔颜色和填充色
+color('pink', 'blue')
+color('#285078', '#234568')
+colormode(1)
+color(0.2, 0.4, 0.6)
+colormode(255)
+color(80, 160, 240)
 
 
 
@@ -168,6 +226,12 @@ radians() 弧度
 - begin_fill() 开始填充
 - end_fill() 结束填充
 '''
+begin_fill() # 在绘制要填充的形状之前调用
+if filling():
+    fillcolor('yellow')
+    circle(80)
+end_fill() # 填充上次调用 begin_fill() 之后绘制的形状
+
 
 
 ### 更多绘图控制
@@ -176,6 +240,19 @@ radians() 弧度
 - clear() 清空
 - write() 书写
 '''
+reset() # 从屏幕中删除海龟的绘图，海龟回到原点并设置所有变量为默认值
+clear() # 从屏幕中删除指定海龟的绘图，不移动海龟; 海龟的状态、位置以及其他海龟的绘图不受影响
+
+'''
+write(arg, move=False, align='left', font=('Arial, 8, 'normal'))
+- arg: 要书写到 TurtleScreen 的对象，指定的字符串到当前海龟位置
+- move: True/False, 为True时，画笔移动到文本的右下角
+- align: 'left', 'center', 'right'
+- font: 一个三元组(fontname, fontsize, fonttype)
+'''
+write('Home=', True, align='center')
+write((0,0), True)
+
 
 
 ## 海龟状态
@@ -186,6 +263,7 @@ radians() 弧度
 - hideturtle() | ht() 隐藏海龟
 - isvisible() 是否可见
 '''
+
 
 
 ### 外观
@@ -200,6 +278,7 @@ radians() 弧度
 - shapetransform() 变形
 - get_shapepoly() 获取形状多边形
 '''
+
 
 
 ## 使用事件
@@ -237,12 +316,15 @@ radians() 弧度
 - setworldcoordinates() 设置世界坐标系
 '''
 
+
+
 ## 动画控制
 '''
 - delay() 延迟
 - tracer() 追踪
 - update() 更新
 '''
+
 
 
 ## 使用屏幕事件
@@ -254,6 +336,7 @@ radians() 弧度
 - ontimer() 当达到定时
 - mainloop | done() 主循环
 '''
+
 
 
 ## 设置与特殊方法
@@ -276,6 +359,7 @@ radians() 弧度
 '''
 
 
+
 ## Screen 专有方法
 '''
 - bye() 退出
@@ -288,6 +372,7 @@ radians() 弧度
 
 
 '''
+# Draw a shape
 color('red', 'yellow')
 begin_fill()
 while True:
