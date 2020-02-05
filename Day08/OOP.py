2dff00ff5e1e90eb1d626c5d504a789774cc4600
shunz <rockucn@gmail.com>
2020-02-06 01:30:32 +0800

    学习：创建和使用对象、访问可见性问题

--- Day08/OOP.py
+++ Day08/OOP.py
@@ -56,36 +56,102 @@ class Student(object):
         self.age = age
 
     def study(self, course_name):
-        print(f'{self.name}正在学习{course_name}.')
+        print(f'{self.name}正在学习{course_name}。')
 
 
+    # PEP 8要求标识符的名字用全小写，多个单词用下划线连接
+    # 但部分程序员和公司更倾向于使用驼峰命名法(驼峰标识)
+    def watch_movie(self):
+        if self.age < 18:
+            print(f'{self.name}只能观看《熊出没》。')
+        else:
+            print(f'{self.name}正在观看岛国爱情大电影。')
 
 
+'''
+创建和使用对象
+'''
+def main():
+    # 创建学生对象并指定姓名和年龄
+    stu1 = Student('Shunz', 39)
+    # 给对象发study消息
+    stu1.study('Python程序设计')
+    # 给对象发watch_movie消息
+    stu1.watch_movie()
+    
+    stu2 = Student('王大锤', 15)
+    stu2.study('思想品德')
+    stu2.watch_movie()
+
+if __name__ == '__main__':
+    main()
 
 
+'''
+访问可见性问题
+
+- Python中属性和方法的访问权限只有两种：公开的(public)、私有的(private)，C++、Java、C#等语言还有受保护的(protected)权限
+- 公开的方法就是对象能够接受的消息
+- 如果希望属性是私有的，给属性命名时用两个下划线作为开头
+- 但 Python 并没有从语法上严格保证私有属性和方法的私密性，只是通过换名字来妨碍对其的访问
+  只要知道换名字的规则，仍然可以访问到。
+- 之所以这么设定，可以用一句名言解释"We are all consenting adults here", 因为绝
+  大多数程序员认为开放比封闭好，而且程序员要为自己的行为负责。
+- 实际开发中并不建议将属性设为私有，会导致子类无法访问，所以大家会遵循一种命名惯例：以单
+  下划线开头表示属性是受保护的，本类之外的代码在访问时应保持慎重。这种做法不是语法上的规
+  定，外界是可以访问的，它更多是一种暗示和隐喻。
+'''
+class Test:
+
+    def __init__(self, foo):
+        # 定义私有属性
+        self.__foo = foo
+        # 定义公开属性
+        self.foo = foo
+
+    # 定义私有方法
+    def __bar(self):
+        print(self.__foo)
+        print('__bar')
+
+    # 定义公开方法
+    def bar(self):
+        print(self.foo)
+
+def main():
+    test = Test('hello')
+    # AtrributeError: 'Test' object has no attribute '__bar'
+    # test.__bar()
+    # AttributeError: 'Test' object has no attribute '__foo'
+    # print(test.__foo)
+
+    # 调用公开属性/方法
+    test.bar()
+    print(test.foo)
+
+    # 通过名字规则访问私有属性/方法
+    test._Test__bar()
+    print(test._Test__foo)
 
 
+if __name__ == '__main__':
+    main()
 
 
+'''
+'foo': a term used for unimportant variables in programming when the
+programmer is too lazy to think of an actual name. The origin of such
+word is described in detail in RFC 3092.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+The terms 'foobar', 'foo', 'bar', and 'baz' are sometimes used as
+placeholder names (also referred to as metasyntactic variables) in
+computer programming or computer-related documentation. They have been
+used to name entities such as variables, functions, and commands whose
+purpose is unimportant and serve only to demonstrate a concept. The
+words themselves have no meaning in this usage. Foobar is sometimes
+used alone; foo, bar, and baz are sometimes used in that order, when
+multiple entities are needed.
+'''
 
 
 
