668823050393dcf564840eb5bf5bfa7d36941ff8
shunz <rockucn@gmail.com>
2020-02-16 01:47:20 +0800

    增加 Python 二级等考的练习内容

---
+++ PythonExam/Chapter1/m1.5PrintLocalDateTime.py
@@ -0,0 +1,7 @@
+"""微实例1.5 日期和时间的输出"""
+
+from datetime import datetime   # 引用datetime库
+now = datetime.now()            # 获得当前日期和时间信息
+print(now)
+print(now.strftime('%x'))       # 输出其中的日期部分
+print(now.strftime('%X'))       # 输出其中的时间部分
